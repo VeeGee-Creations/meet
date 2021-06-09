5a8f4bdfed9c5cd3a4dd6b184bbc37df56d0e9e3
VeeGee-Creations <vgeesman@gmail.com>
2021-06-09 03:30:44 -0700

    added alerts using OOP

--- src/components/CitySearch/CitySearch.js
+++ src/components/CitySearch/CitySearch.js
@@ -1,27 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { InfoAlert } from '../Alert/Alert';
 
 export default class CitySearch extends Component {
     state = {
-        query:'',
+        query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: false,
+        infoText: ''
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        })
-        this.setState({
-            query: value,
-            suggestions,
         });
+        console.log(suggestions.length);
+        if (suggestions.length === 0) {
+            this.setState({
+                query: value,
+                suggestions,
+                infoText: 'We can not find the city you are looking for. Please try another city',
+            });
+        } else {
+            this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            });
+        }
     }
 
     handleItemClicked = (suggestion) => {
         this.setState({
             query: suggestion,
-            showSuggestions: false
+            showSuggestions: false,
+            infoText: ''
         });
 
         this.props.updateEvents(suggestion);
@@ -31,6 +44,8 @@ export default class CitySearch extends Component {
         const {query, suggestions, showSuggestions} = this.state;
         return (
             <div className="CitySearch">
+                <InfoAlert text={this.state.infoText} />
+                <div className="SearchContainer">
                 <label>Cities:
                 <input type="text"
                 className="city"
@@ -49,6 +64,7 @@ export default class CitySearch extends Component {
                         <b>See all cities</b>
                     </li>
                 </ul>
+                </div>
             </div>
         );
     }
--- src/components/NumberOfEvents/NumberOfEvents.js
+++ src/components/NumberOfEvents/NumberOfEvents.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react';
+import { ErrorAlert } from '../Alert/Alert';
 
 export default class NumberOfEvents extends Component {
     state = {
         resultNumber: 32,
+        errorText: ''
     }
 
     handleInputChange = (event) => {
         const value = event.target.value;
-        this.setState({
-            resultNumber: value
-        });
+        switch(true) {
+            case value < 1:
+                this.setState({
+                    resultNumber: 1,
+                    errorText: 'Select number from 1 to 32'
+                });
 
-        this.props.updateNumberOfEvents(event.target.value);
+                this.props.updateNumberOfEvents(1);
+                break;
+            case value > 32:
+                this.setState({
+                    resultNumber: 32,
+                    errorText: 'Select number from 1 to 32'
+                });
+
+                this.props.updateNumberOfEvents(32);
+                break;
+            default:
+                this.setState({
+                    resultNumber: value,
+                    errorText: ''
+                });
+
+                this.props.updateNumberOfEvents(event.target.value);
+                break;
+        }
     }
 
     render() {
@@ -23,8 +46,11 @@ export default class NumberOfEvents extends Component {
                 className="number-events"
                 value={resultNumber}
                 onChange={this.handleInputChange}
+                min="1"
+                max="32"
                 />
                 </label>
+                <ErrorAlert text={this.state.errorText} />
             </div>
         );
     }
